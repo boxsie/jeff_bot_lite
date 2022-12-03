6a0b49d8b217c26ca5ac085a977e012557b2fd4d
sketcheon <gg@fun.com>
2022-12-03 14:28:33 +0000

    Added Hours & Seconds

--- commands/fun.py
+++ commands/fun.py
@@ -1,4 +1,5 @@
 import discord
+import math
 from discord.ext import commands
 from datetime import datetime, date
 
@@ -13,13 +14,15 @@ async def friday(ctx):
       
 @commands.command(name='xmas', help='Xmas Countdown')
 async def xmas(ctx):
-    today = date.today()
-    xmas_day = date(today.year, 12, 25)
+    today = datetime.today()
+    xmas_day = datetime(today.year, 12, 25)
     delta = xmas_day - today
     if delta.days < 0:
-        await ctx.send(f"🎄🎄🎄 It's bin 'n gone mate {str(kekwx)} 🎄🎄🎄")
+        await ctx.send(f"🎄🎄🎄 It's bin 'n gone mate 🎄🎄🎄")
     elif delta.days == 0: 
         await ctx.send(f"🎄🎄🎄 Merry Xmas 🎄🎄🎄 \nhttps://youtu.be/pHMhEWyqj2g?t=75")
     else:    
-        await ctx.send(f"🎄🎄🎄 It's {delta.days} days until Xmas 🎄🎄🎄")
+        hours = int(delta.seconds // (60 * 60))
+        mins = int((delta.seconds // 60) % 60)  
+        await ctx.send(f"🎄🎄🎄 It's {delta.days} days, {hours} hours & {mins} minutes until Xmas !! 🎄🎄🎄")
         
